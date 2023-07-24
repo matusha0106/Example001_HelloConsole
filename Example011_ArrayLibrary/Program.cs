f62a9d660a97c9356610efa9763ece4c5c00489d
Anastasiya <podik0106@icloud.com>
2023-07-25 00:32:27 +0300

    Add Example011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index = index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0; 
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+        //return не используется в коде, если есть void - метод, который ничего не возвращает
+    }
+}
+int [] array = new int [10]; //создай новый массив из 10 элекментов
+
+FillArray(array);
+PrintArray(array);
